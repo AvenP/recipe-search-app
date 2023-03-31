a256e028d12253180da25f01d198870e36ef2b98
wren <zennguyen2511@gmail.com>
2023-03-30 19:13:00 -0500

    fix display recipe

--- assets/script.js
+++ assets/script.js
@@ -15,22 +15,34 @@ document.addEventListener('DOMContentLoaded', () => {
   function displayRecipes(recipes) {
     const resultsContainer = document.getElementById('results-container');
     resultsContainer.innerHTML = '';
-
+  
     recipes.forEach(recipe => {
       const recipeCard = document.createElement('div');
       recipeCard.classList.add('recipe-card');
-
+  
       const title = document.createElement('h3');
       title.innerText = recipe.title;
-
+  
       const image = document.createElement('img');
       image.src = `https://spoonacular.com/recipeImages/${recipe.image}`;
-
+  
+      const description = document.createElement('p');
+      if (recipe.summary) {
+        description.innerText = recipe.summary;
+      } else if (recipe.instructions) {
+        description.innerText = recipe.instructions;
+      } else {
+        description.innerText = 'No recipe available.';
+      }
+  
       recipeCard.appendChild(title);
       recipeCard.appendChild(image);
+      recipeCard.appendChild(description);
       resultsContainer.appendChild(recipeCard);
     });
   }
+  
+  
 
   // Function to search for YouTube videos
   async function searchVideos(query) {
@@ -44,30 +56,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Function to display video results
   function displayVideos(videos) {
-    const resultsContainer = document.getElementById('results-container');
-    resultsContainer.innerHTML = '';
-
+    const videoResultsContainer = document.getElementById('video-results-container');
+    videoResultsContainer.innerHTML = '';
+  
     videos.forEach(video => {
       const videoCard = document.createElement('div');
       videoCard.classList.add('video-card');
-
+  
       const title = document.createElement('h3');
       title.innerText = video.snippet.title;
-
+  
       const thumbnail = document.createElement('img');
       thumbnail.src = video.snippet.thumbnails.medium.url;
-
+  
       const videoLink = document.createElement('a');
       videoLink.href = `https://www.youtube.com/watch?v=${video.id.videoId}`;
       videoLink.target = '_blank';
       videoLink.innerText = 'Watch on YouTube';
-
+  
       videoCard.appendChild(title);
       videoCard.appendChild(thumbnail);
       videoCard.appendChild(videoLink);
-      resultsContainer.appendChild(videoCard);
+      videoResultsContainer.appendChild(videoCard);
     });
   }
+  
 
   // Event listener for search button
   const searchButton = document.getElementById('search-button');
