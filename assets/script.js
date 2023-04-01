0ff8bf22f23fa6ca657b4db3a0e3b4e6bfd00ed1
wren <zennguyen2511@gmail.com>
2023-04-01 13:19:04 -0500

    fix bug in js

--- assets/script.js
+++ assets/script.js
@@ -97,6 +97,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-/* APIs
-Rojan's API key: FJVk2YSVm3Vi9Pg1Tv99XarbQ2Vlqv1k
-(500 calls a month for Youtube and 3,000 for spoonacular)*/
